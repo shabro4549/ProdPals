887069e2af47d6493e89a3c024233cca45933f7e
Shannon Brown <shabro4549@gmail.com>
2021-07-08 16:39:13 -0700

    Add ChatViewController
    Add second tab to tab bar controller and connect to ChatViewController

---
+++ Prod Pals/Controllers/ChatViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ChatViewController.swift
+//  Something
+//
+//  Created by Shannon Brown on 2021-07-08.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var ifEmptyLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
