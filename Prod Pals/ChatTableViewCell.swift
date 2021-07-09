52a3b5287ca6ff766e405a1c87498f2138fc9ed8
Shannon Brown <shabro4549@gmail.com>
2021-07-08 20:43:28 -0700

    Set up chatTableView in ChatViewController
    Add ChatTableViewCell.swift and xib, set up to render in ChatViewController

---
+++ Prod Pals/ChatTableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  ChatTableViewCell.swift
+//  Something
+//
+//  Created by Shannon Brown on 2021-07-08.
+//
+
+import UIKit
+
+class ChatTableViewCell: UITableViewCell{
+    
+    static let identifier = "ChatTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "ChatTableViewCell", bundle: nil)
+    }
+    
+    @IBOutlet weak var chatButton: UIButton!
+    
+    func configure(with title: String) {
+        print(title)
+        chatButton.setTitle(title, for: .normal)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20))
+        
+    }
+    
+}
--- Prod Pals/Controllers/ChatViewController.swift
+++ Prod Pals/Controllers/ChatViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var ifEmptyLabel: UILabel!
+    @IBOutlet weak var chatTableView: UITableView!
+    var goalColours = [#colorLiteral(red: 1, green: 0.6235294118, blue: 0.9529411765, alpha: 1), #colorLiteral(red: 0.9960784314, green: 0.7921568627, blue: 0.3411764706, alpha: 1), #colorLiteral(red: 1, green: 0.4196078431, blue: 0.4196078431, alpha: 1), #colorLiteral(red: 0.2823529412, green: 0.8588235294, blue: 0.9843137255, alpha: 1), #colorLiteral(red: 0.1137254902, green: 0.8196078431, blue: 0.631372549, alpha: 1), #colorLiteral(red: 0, green: 0.8235294118, blue: 0.8274509804, alpha: 1), #colorLiteral(red: 0.3294117647, green: 0.6274509804, blue: 1, alpha: 1), #colorLiteral(red: 0.3725490196, green: 0.1529411765, blue: 0.8039215686, alpha: 1)]
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        chatTableView.register(ChatTableViewCell.nib(), forCellReuseIdentifier: ChatTableViewCell.identifier)
+        chatTableView.delegate = self
+        chatTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
@@ -29,3 +36,24 @@ class ChatViewController: UIViewController {
     */
 
 }
+
+//MARK: - UITableView Delegate & Data Source Methods
+
+extension ChatViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = chatTableView.dequeueReusableCell(withIdentifier: "ChatTableViewCell", for: indexPath) as! ChatTableViewCell
+
+        cell.configure(with: "Chats")
+        cell.chatButton.backgroundColor = goalColours.randomElement()
+        cell.chatButton.layer.cornerRadius = 20
+        cell.chatButton.layer.masksToBounds = true
+        return cell
+    }
+    
+    
+}
+
